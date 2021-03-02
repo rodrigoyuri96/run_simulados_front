bc75199d6a4125ce9b4fe620c8935295afc701f6
david.sabino17@gmail.com <david.sabino17@gmail.com>
2021-03-02 18:17:20 -0300

    Tela do cadatro de evento, com os modais

--- src/components/run/Date.vue
+++ src/components/run/Date.vue
@@ -1,82 +1,150 @@
 <template>
   <div>
-    <v-menu
-      ref="menu1"
-      v-model="menu1"
-      :close-on-content-click="false"
-      transition="scale-transition"
-      offset-y
-      max-width="290px"
-      min-width="auto"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-text-field
-          v-model="dateFormatted"
-          :label="label"
-          hint="formato: MM/DD/YYYY"
-          outlined
-          dense
-          required
-          append-icon="mdi-calendar"
-          v-bind="attrs"
-          @blur="date = parseDate(dateFormatted)"
-          v-on="on"
-        ></v-text-field>
-      </template>
-      <v-date-picker
-        v-model="date"
-        no-title
-        @input="menu1 = false"
-      ></v-date-picker>
-    </v-menu>
+    <v-row>
+      <v-col cols="6" lg="6">
+        <v-menu
+          ref="menu1"
+          v-model="menu1"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          max-width="290px"
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="dateFormatted"
+              label="Data inicio inscrição"
+              hint="formato: MM/DD/YYYY"
+              outlined
+              dense
+              required
+              prepend-icon="mdi-calendar"
+              v-bind="attrs"
+              @blur="date = parseDate(dateFormatted)"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="date"
+            no-title
+            @input="menu1 = false"
+          ></v-date-picker>
+        </v-menu>
+      </v-col>
+
+      <v-col cols="6" lg="6">
+        <v-menu
+          ref="menu2"
+          v-model="menu2"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          max-width="290px"
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="dateFormatted"
+              label="Data fim inscrição"
+              hint="formato: MM/DD/YYYY"
+              outlined
+              dense
+              required
+              prepend-icon="mdi-calendar"
+              v-bind="attrs"
+              @blur="date = parseDate(dateFormatted)"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="date"
+            no-title
+            @input="menu2 = false"
+          ></v-date-picker>
+        </v-menu>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6" lg="6">
+        <v-menu
+          ref="menu3"
+          v-model="menu3"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          max-width="290px"
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="dateFormatted"
+              label="Data inicio do evento"
+              hint="formato: MM/DD/YYYY"
+              outlined
+              dense
+              required
+              prepend-icon="mdi-calendar"
+              v-bind="attrs"
+              @blur="date = parseDate(dateFormatted)"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="date"
+            no-title
+            @input="menu3 = false"
+          ></v-date-picker>
+        </v-menu>
+      </v-col>
+      <v-col cols="6" lg="6">
+        <v-menu
+          ref="menu4"
+          v-model="menu4"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          max-width="290px"
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="dateFormatted"
+              label="Data fim do evento"
+              hint="formato: MM/DD/YYYY"
+              outlined
+              dense
+              required
+              prepend-icon="mdi-calendar"
+              v-bind="attrs"
+              @blur="date = parseDate(dateFormatted)"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="date"
+            no-title
+            @input="menu4 = false"
+          ></v-date-picker>
+        </v-menu>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Watch, VModel, Prop } from "vue-property-decorator";
+import { Vue, Component } from "vue-property-decorator";
+import { getModule } from "vuex-module-decorators";
+import {DateModule}  from  "@/store/modules/DateModule";
+
 
 @Component({
-  name: "RunDate",
+  name: "Date"
 })
-export default class RunDate extends Vue {
-  menu1 = false;
-  @VModel({type: String}) date: String | "";
-  @Prop({type: String}) label: String | "";
-  //.toISOString().substr(0, 10);
-  dateFormatted: String = new String();
-  //vm.formatDate(new Date().toISOString().substr(0, 10));
 
-  get computedDateFormatted() {
-    return this.formatDate(this.date);
-  }
-
-  @Watch("date")
-  WhenDateChanged(val: String) {
-    this.dateFormatted = this.formatDate(val);
-  }
-
-  formatDate(date: String): String {
-    if (!date) return "";
-    const [year, month, day] = date.split("-");
-    return `${day}/${month}/${year}`;
-  }
-
-  parseDate(date: String) {
-    if (!date) return null;
-    const [month, day, year] = date.split("/");
-    return `${year}-${day.padStart(2, "0")}-${month.padStart(2, "0")}`;
-  }
-
-  mounted() {
-    let date: Date = new Date();
-    let str = `${date.getFullYear()}-${date
-      .getDay()
-      .toString()
-      .padStart(2, "0")}-${date.getMonth().toString().padStart(2, "0")}`;
-    this.date = str;
-    console.log(str);
-  }
-}
+export default class Date extends Vue {
+dateModule = getModule(DateModule, this.$store);
+};
 </script>
 
 <style>
